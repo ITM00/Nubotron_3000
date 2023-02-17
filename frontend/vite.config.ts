b9c2df55e9b481984898348bbec51d1f64909842
cairon <egorfrolov2019.ef@gmail.com>
2023-02-17 18:30:34 +0300

    fix nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ const build = path.join(process.cwd(), 'build');
 
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
-    base: './',
+    base: '/',
     plugins: [
         react(),
         visualizer({
