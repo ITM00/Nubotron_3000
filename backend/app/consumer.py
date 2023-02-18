4555f5f6db90bb89f3da97360297ff48dda938be
Ivan A <1111000110@mail.ru>
2023-02-18 23:43:15 +0300

    midnight fever

--- backend/app/consumer.py
+++ backend/app/consumer.py
@@ -1,20 +1,16 @@
-import json
 import psycopg2
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
-conn = psycopg2.connect(
+
+def add_data_in_db(date_time, s):
+    conn = psycopg2.connect(
         host='db',
         port=5432,
         database="postgres",
         user="postgres",
         password="postgres",
-)
-
-def add_data_in_db(json):
-
+    )
     cur = conn.cursor()
-    cur.execute("INSERT INTO consumer_data (d_create, json) VALUES(current_timestamp, (%s))", (json,))
+    cur.execute(f"INSERT INTO consumer_data (d_create, data) VALUES('{date_time}', '{s}')")
 
     conn.commit()
     conn.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 from kafka import KafkaConsumer, TopicPartition
@@ -8,7 +7,8 @@ import asyncio
 from pydantic import BaseModel, StrictStr
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from aiokafka.helpers import create_ssl_context
-from .consumer import add_data_in_db, conn
+from .consumer import add_data_in_db
+from .pull_history import pull_history
 
 
 app = FastAPI()
@@ -17,7 +17,7 @@ topic = 'zsmk-9433-dev-01'
 loop = asyncio.get_event_loop()
 
 context = create_ssl_context(
-    cafile="CA.crt"
+    cafile="app/CA.crt"
 )
 
 consumer = AIOKafkaConsumer(
@@ -39,14 +39,16 @@ async def consume():
             my_bytes_value = msg.value
             my_json = my_bytes_value.decode('utf8').replace("'", '"')
             data = json.loads(my_json)
+            date_time = str(data['moment'].replace('T', " ").split(".")[0])
             s = json.dumps(data, indent=4, sort_keys=True)
-            add_data_in_db(s)
+            add_data_in_db(date_time, s)
     finally:
         await consumer.stop()
 
 
 @app.on_event("startup")
 async def startup_event():
+    await pull_history(topic)
     loop.create_task(consume())
 
 
@@ -55,9 +57,27 @@ async def shutdown_event():
     await consumer.stop()
 
 
-@app.get("/api/get_all_data")
-def get_all_data(request: Request) -> dict[str, int]:
+"/api/get_all_data/2023-02-18T18:17:31.749Z&2023-02-18T18:17:31.749Z&1h"
+@app.get("/api/get_all_data/{start}{end}{interval}")
+def get_all_data(request: Request) -> dict[str, float]:
     """Тут отдаем исторические данные"""
+    conn = psycopg2.connect(
+        host='db',
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="postgres",
+    )
+    start_date = start.replace("T", " ")[:16]
+    finish_date = end.replace("T", " ")[:16]
+    step = int(interval) if interval.endswith("m") else int(interval) * 60
+    cur.execute(f"SELECT id from consumer_data where d_create::text like {start_date}")
+    satrt_id = cur.fetchall()[0][0]
+    cur.execute(f"SELECT id from consumer_data where d_create::text like {finish_date}")
+    max_id = cur.fetchall()[0][0]
+    ids = [range(satrt_id, max_id+1, step)]
+    cur.execute(f"SELECT data from consumer_data where id in {ids}")
+
     return {"request": 200}
 
 
@@ -75,6 +95,5 @@ def get_current_data():
     cur.execute("SELECT id, json FROM consumer_data ORDER BY consumer_data.id DESC LIMIT 1")
     data = cur.fetchall()
     conn.close()
-    print(data)
     cur.close()
     return {"data": data}
