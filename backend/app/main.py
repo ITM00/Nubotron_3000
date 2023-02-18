72cfd6cf18fc58163d36b59475b1aab868f08bcb
mihmih <workmihmih@gmail.com>
2023-02-18 16:29:52 +0300

    кафка фоном

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,77 @@
-from fastapi import FastAPI, Request
-# from fastapi.templating import Jinja2Templates
 
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.templating import Jinja2Templates
+from kafka import KafkaConsumer, TopicPartition
+import json
+import psycopg2
+import asyncio
+from pydantic import BaseModel, StrictStr
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from aiokafka.helpers import create_ssl_context
+from .consumer import add_data_in_db, conn
 
 app = FastAPI()
-# templates = Jinja2Templates(directory="templates")
+
+topic = 'zsmk-9433-dev-01'
+loop = asyncio.get_event_loop()
+
+context = create_ssl_context(
+    cafile="CA.crt"
+)
+
+consumer = AIOKafkaConsumer(
+    topic,
+    group_id='Nubotron_3000',
+    bootstrap_servers='rc1a-b5e65f36lm3an1d5.mdb.yandexcloud.net:9091',
+    security_protocol="SASL_SSL",
+    sasl_mechanism="SCRAM-SHA-512",
+    sasl_plain_username='9433_reader',
+    sasl_plain_password='eUIpgWu0PWTJaTrjhjQD3.hoyhntiK',
+    ssl_context=context,
+)
+
+async def consume():
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            my_bytes_value = msg.value
+            my_json = my_bytes_value.decode('utf8').replace("'", '"')
+            data = json.loads(my_json)
+            s = json.dumps(data, indent=4, sort_keys=True)
+            add_data_in_db(s)
+    finally:
+        await consumer.stop()
+
+@app.on_event("startup")
+async def startup_event():
+    loop.create_task(consume())
 
 
-@app.get("/")
-def home() -> dict[str, str]:
-    return {"status": "Success 222"}
+@app.on_event("shutdown")
+async def shutdown_event():
+    await consumer.stop()
+
+
+@app.get("/api/get_all_data")
+def get_all_data(request: Request) -> dict[str, int]:
+    "тут отдаем исторические данные"
+    return {"request": 200}
+
+
+@app.get("/api/get_current_data")
+def get_current_data():
+    "тут получаем актуальный последний из кафки и отдаем на фронт"
+    conn = psycopg2.connect(
+        host='db',
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="postgres",
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT id, json FROM consumer_data ORDER BY consumer_data.id DESC LIMIT 1")
+    data = cur.fetchall()
+    conn.close()
+    print(data)
+    cur.close()
+    return {"data": data}
